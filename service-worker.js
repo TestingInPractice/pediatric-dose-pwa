42c8a2654e5a8446a6b3c25853012445c6ed4170
sabundhal <sabundhal@gmail.com>
2026-05-08 01:01:14 +0300

    phase 1: PWA-каркас (index.html, manifest, service-worker, CSS, icons)

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'dose-calc-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/css/style.css',
+  '/js/calculator.js',
+  '/js/level2_rules.js',
+  '/js/level4_images.js',
+  '/js/db.js',
+  '/js/updater.js',
+  '/js/app.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+
+  if (event.request.method !== 'GET') return;
+
+  if (url.pathname.startsWith('/data/')) {
+    event.respondWith(
+      fetch(event.request)
+        .then((res) => {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+          return res;
+        })
+        .catch(() => caches.match(event.request))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
